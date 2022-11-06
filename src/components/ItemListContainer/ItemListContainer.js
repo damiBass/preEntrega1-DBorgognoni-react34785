60f0f3772d49434096cb9a2a5e806809a62d3995
DBorgognoni <dborgognoni@makingsense.com>
2022-11-06 15:54:39 -0300

    practice promise

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,6 +2,16 @@ import React from "react";
 import ItemCard from "../ItemCard/ItemCard";
 import { dataProducts } from "../../data/data";
 
+function APICall() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(dataProducts);
+    }, 2000);
+  });
+}
+
+APICall().then((resp) => console.log(resp));
+
 function ItemListContainer() {
   return (
     <div
